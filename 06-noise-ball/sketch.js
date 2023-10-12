cfb95cf6b96b54f227c7036a407b2c9b21c9fb8c
JeremyTratch <jeremy.tratch@gmail.com>
2023-10-12 12:10:15 -0600

    created noise demo

---
+++ 06-noise-ball/sketch.js
@@ -0,0 +1,44 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+ 
+let ballArray = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background("white");
+  spawnBall();
+}
+
+function draw() {
+  noStroke();
+  for (let theBall of ballArray) {
+    fill(theBall.color);
+    //move
+    theBall.x = noise(theBall.time) * width;
+    theBall.y = noise(theBall.time + 300) * height;
+    //display
+    circle(theBall.x, theBall.y, theBall.size)
+  
+    theBall.time += 0.01;
+  }
+}
+
+function mousePressed() {
+  spawnBall();
+}
+
+function spawnBall() {
+  let ball = {
+    x: random(width),
+    year: random(height),
+    size: random(10, 50),
+    color: color(random(255), random(255), random(255), (random(255))),
+    time: random(1000),
+  };
+  ballArray.push(ball);
+}
--- index.md
+++ index.md
@@ -5,6 +5,7 @@
 - [Bouncing Ball](01-ball)
 - [Ball Oject Notation](03-ball-object)
 - [Ball Array](05-ball-array)
+- [Perlin Noise](06-noise-ball)
 
 ## Projects
 -[Arrays/object assignment](04 - Arrays assignment)
