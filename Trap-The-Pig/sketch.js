85c9e3063b9461fb477511c61983024d1c1012b2
JeremyTratch <jeremy.tratch@gmail.com>
2023-10-31 11:45:27 -0600

    started project

--- Trap-The-Pig/sketch.js
+++ Trap-The-Pig/sketch.js
@@ -1,4 +1,4 @@
-// Trap The Pig
+// Block Catch
 // Jeremy Tratch
 // October 27, 2023
 //
@@ -7,11 +7,16 @@
 
 let grid;
 let cellSize;
-const GRID_SIZE = 7;
+const GRID_SIZE = 40;
+let playerX = 0;
+let playerY = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = genRandGrid(GRID_SIZE, GRID_SIZE);
+
+  grid[playerY][playerX] = 9;
 
   if (height > width) {
     cellSize = width/GRID_SIZE;
@@ -26,6 +31,42 @@ function draw() {
   displayGrid();
 }
 
+function keyTyped() {
+  if (key === "r") {
+    grid = genRandGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === "s") {
+    movePlayer(0, 1);
+  }
+  else if (key === "w") {
+    movePlayer (0, -1);
+  }
+  else if (key === "a") {
+    movePlayer(-1, 0);
+  }
+  else if (key === "d") {
+    movePlayer(1, 0);
+  }
+}
+
+function movePlayer(x, y) {
+  if (playerX + x >= 0 && playerX + x < GRID_SIZE && playerY + y >= 0 && playerY + y < GRID_SIZE) {
+    // check for wall
+    if (grid[playerY + y][playerX + x] === 0) {
+      let tempX = playerX;
+      let tempY = playerY;
+
+      playerX += x;
+      playerY += y;
+
+      //update grid
+      grid[playerY][playerX] = 9;
+      grid[tempY][tempX] = 0;
+    }
+  }
+}
+
+
 function mousePressed() {
   let y = Math.floor(mouseY/cellSize);
   let x = Math.floor(mouseX/cellSize);
@@ -34,7 +75,7 @@ function mousePressed() {
   // toggleCell(x, y - 1); // north neigbour
   // toggleCell(x, y + 1); // south
   // toggleCell(x + 1, y); // east
-  //  toggleCell(x - 1, y); // west
+  // toggleCell(x - 1, y); // west
 }
 
 function toggleCell(x, y) {
@@ -57,6 +98,9 @@ function displayGrid() {
       else if (grid[y][x] === 1) {
         fill("black");
       }
+      else if (grid[y][x] === 9) {
+        fill("green");
+      }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
@@ -67,7 +111,7 @@ function genRandGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 70) {
+      if (random(100) < 50) {
         newGrid[y].push(0);
       }
       else {
@@ -76,9 +120,4 @@ function genRandGrid(cols, rows) {
     }
   }  
   return newGrid;
-}
-
-
-function spawnPig() {
-
 }
